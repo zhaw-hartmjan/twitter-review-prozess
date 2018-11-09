e8c26fc33a188878e9a6a3e1761498c2aed933ec
Jan Hartmann <hartmjan@students.zhaw.ch>
2018-11-09 20:13:54 +0100

    Bis und mit 8.2 fertig

--- src/main/java/ch/zhaw/gpi/twitterreview/delegates/SendTweetDelegate.java
+++ src/main/java/ch/zhaw/gpi/twitterreview/delegates/SendTweetDelegate.java
@@ -4,10 +4,11 @@
  * and open the template in the editor.
  */
 package ch.zhaw.gpi.twitterreview.delegates;
-
+import ch.zhaw.gpi.twitterreview.services.TwitterService;
 import javax.inject.Named;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Implementation des Service Tasks "Tweet senden"
@@ -15,19 +16,24 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
  * @author janhartmann
  */
 @Named("sendTweetAdapter")
-
 public class SendTweetDelegate implements JavaDelegate {
+    
+    //verdrahten von TwitterSerci
+    @Autowired
+    private TwitterService twitterService;
+/**
+* Postet einen Tweet mit dem gewünschten Text
+*
+* @param de Objekt, welches die Verknüpfung zur Process Engine und zur aktuellen Execution enthält * @throws Exception
+*/
+@Override
+public void execute(DelegateExecution de) throws Exception {
+    
 
-    /**
-     * 1. Die Prozessvariable tweetContent wird ausgelesen
-     * 2. Dieser Text wird in der Konsole ausgeben
-     * @param execution   Objekt welches die Verknüpfung zur Process Engine und aktuellen Execution enthält
-     * @throws Exception
-     */
-    @Override
-    public void execute(DelegateExecution execution) throws Exception {
-        String tweetContent = (String) execution.getVariable("tweetContent");
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!! Folgender Tweet wird veröffentlicht: " + tweetContent);
-    }
+// Prozessvariable tweetContent wird ausgelesen
+String tweetContent = (String) de.getVariable("tweetContent");
+// Dieser Text wird dem Twitter Service an die Methode updateStatus übergeben
+twitterService.updateStatus(tweetContent);
+}
     
 }
